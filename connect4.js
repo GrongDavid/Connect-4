bed03d4c8df73b585948a5eac9531de46702b556
David <davidgrong778@gmail.com>
2022-03-09 18:24:38 -0600

    working connect four!

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
- var WIDTH = 7;
- var HEIGHT = 6;
+ const WIDTH = 7;
+ const HEIGHT = 6;
  
- var currPlayer = 1; // active player: 1 or 2
- var board = []; // array of rows, each row is array of cells  (board[y][x])
+ let currPlayer = 1; // active player: 1 or 2
+ const board = []; // array of rows, each row is array of cells  (board[y][x])
  
  /** makeBoard: create in-JS board structure:
   *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,20 +17,27 @@
  
  function makeBoard() {
    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+   for(let i = 0; i < HEIGHT; i++){
+        board.push([]);
+   }
+   for(elements of board){
+       elements.push(null);
+   }
+   console.log(board);
  }
  
  /** makeHtmlBoard: make HTML table and row of column tops. */
  
  function makeHtmlBoard() {
    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
- 
+    const htmlBoard = document.querySelector("#board");
    // TODO: add comment for this code
-   var top = document.createElement("tr");
+   const top = document.createElement("tr");
    top.setAttribute("id", "column-top");
    top.addEventListener("click", handleClick);
  
    for (var x = 0; x < WIDTH; x++) {
-     var headCell = document.createElement("td");
+     const headCell = document.createElement("td");
      headCell.setAttribute("id", x);
      top.append(headCell);
    }
@@ -52,26 +59,40 @@
  
  function findSpotForCol(x) {
    // TODO: write the real version of this, rather than always returning 0
-   return 0;
+   for(let y = HEIGHT - 1; y >= 0; y--){
+       if(!board[y][x]){
+           return y;
+       }
+   }
+   return null;
  }
  
  /** placeInTable: update DOM to place piece into HTML table of board */
  
  function placeInTable(y, x) {
    // TODO: make a div and insert into correct table cell
+   const piece = document.createElement("div");
+   const playerString = `p${currPlayer}`;
+   
+   piece.classList.add("piece");
+   piece.id = playerString;
+
+   const correctSpot = document.getElementById(`${y}-${x}`);
+   correctSpot.append(piece);
  }
  
  /** endGame: announce game end */
  
  function endGame(msg) {
    // TODO: pop up alert message
+   alert(msg);
  }
  
  /** handleClick: handle click of column top to play piece */
  
  function handleClick(evt) {
    // get x from ID of clicked cell
-   var x = +evt.target.id;
+   let x = +evt.target.id;
  
    // get next spot in column (if none, ignore click)
    var y = findSpotForCol(x);
@@ -82,6 +103,7 @@
    // place piece in board and add to HTML table
    // TODO: add line to update in-memory board
    placeInTable(y, x);
+   board[y][x] = currPlayer;
  
    // check for win
    if (checkForWin()) {
@@ -90,9 +112,22 @@
  
    // check for tie
    // TODO: check if all cells in board are filled; if so call, call endGame
+   if(board.every((row) => row.every((cell) => cell))) {
+       return endGame("It's a tie");`                                                                                                                                                                                                           `
+   }
  
    // switch players
    // TODO: switch currPlayer 1 <-> 2
+   /*
+   if(currPlayer === 1){
+       currPlayer = 2;
+   }
+   else{
+       currPlayer === 1;
+   }
+   */
+   currPlayer = currPlayer === 1 ? 2 : 1;
+
  }
  
  /** checkForWin: check board cell-by-cell for "does a win start here?" */
